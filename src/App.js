d8eb8a941c09bfc7c45082c3f8c817542eda965e
Orivelton Cesar <orivelton10@gmail.com>
2019-12-01 22:12:56 +0000

    chore: API

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import './App.css';
 
 const  App = () => {
+  const APP_ID = 'a7830b99';
+  const APP_KEY = 'b9725f1b13ed41ae9f2e653bddd60409';
   return (
     <div className="App">
       <h1>Hello React</h1>
