91aa3015561c177d13b210eb5d8bd5981ebc6388
Orivelton Cesar <orivelton10@gmail.com>
2019-12-02 23:25:42 +0000

    chore: build

---
+++ precache-manifest.2954bc988aa7362907da90988097dc34.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5461f1970a20191e23b14d06fdfd62ea",
+    "url": "/recipe-app-with-react/index.html"
+  },
+  {
+    "revision": "7448a6601f4c3f65cd02",
+    "url": "/recipe-app-with-react/static/css/main.6773c808.chunk.css"
+  },
+  {
+    "revision": "35e4edd3d82e2c5e696e",
+    "url": "/recipe-app-with-react/static/js/2.30509456.chunk.js"
+  },
+  {
+    "revision": "7448a6601f4c3f65cd02",
+    "url": "/recipe-app-with-react/static/js/main.bc4ddbd8.chunk.js"
+  },
+  {
+    "revision": "d379f5098d2f6ff1f6a9",
+    "url": "/recipe-app-with-react/static/js/runtime-main.495e96a2.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.752d7d9436dba6ad6b377c383926e99a.js"
+  "/recipe-app-with-react/precache-manifest.2954bc988aa7362907da90988097dc34.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/recipe-app-with-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
