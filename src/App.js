309fb9a989edf4205685f055af21b5fa8f0fde7e
Orivelton Cesar <orivelton10@gmail.com>
2019-12-02 21:50:06 +0000

    chore: new component Recipe

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { useEffect, useState} from 'react';
+import Recipe from './Recipe';
 import './App.css';
 
 const  App = () => {
   const APP_ID = 'a7830b99';
   const APP_KEY = 'b9725f1b13ed41ae9f2e653bddd60409';
 
-  const [counter, setCounter] = useState(0);
+  const [recipes, setRecipes] = useState([]);
 
   useEffect(() => {
      getRecipes();
@@ -14,15 +15,21 @@ const  App = () => {
   const getRecipes = async () => {
     const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
     const data = await response.json();
-    console.log(data);
+    console.log(data.hits);
+    setRecipes(data.hits);
   };
 
   return (
     <div className="App">
       <form className="search-form">
         <input className="search-bar" type="text" />
-        <button onClick={() => {setCounter(counter + 1)}} className="search-button">{counter}</button>
+        <button className="search-button">Search</button>
       </form>
+      {
+        recipes.map(recipe => (
+          <Recipe />
+        ))
+      }
     </div>
   );
 }
---
+++ src/Recipe.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Recipe = () => {
+  return(
+    <>
+      <h1>Title</h1>
+      <p>Calories</p>
+      <img src="" alt="" />
+    </>
+  );
+}
+
+export default Recipe;
