cdd5c6df5198dfe8b534f30a8b12520744faaa71
Orivelton Cesar <orivelton10@gmail.com>
2019-12-02 23:15:55 +0000

    chore: build

---
+++ precache-manifest.752d7d9436dba6ad6b377c383926e99a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8ffdf30a302a6f2e6ff0c87ade70b96",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b3668d2584e8a471df43",
+    "url": "/static/css/main.6773c808.chunk.css"
+  },
+  {
+    "revision": "35e4edd3d82e2c5e696e",
+    "url": "/static/js/2.30509456.chunk.js"
+  },
+  {
+    "revision": "b3668d2584e8a471df43",
+    "url": "/static/js/main.534cfe73.chunk.js"
+  },
+  {
+    "revision": "e2a943e3c16599976ebf",
+    "url": "/static/js/runtime-main.bc74d5e7.js"
+  }
+]);
